b9c4a9349a25dd0444919433d08556041bc4ac9b
gkarthik <xraydiffraction@gmail.com>
2022-07-27 12:25:31 -0700

    Added eval script. Read in outputs

---
+++ transcluster/evaluate_results.R
@@ -0,0 +1,26 @@
+library(plyr)
+
+read_input <- function(fname){
+    lines <- readLines(fname)
+    ctr <- 0
+    rows <- lapply(lines, function(x){
+        seq_ids <- strsplit(x, ",")[[1]]
+        tmp <- data.frame(seq = seq_ids)
+        tmp$cluster <- ctr 
+        ctr <- ctr + 1
+        tmp
+    })
+
+    df <- do.call("rbind", rows)
+    snp_threshold <- df[1:2,]
+    df <- df[3:nrow(df),]
+    df["threshold"] <- snp_threshold[1,1]
+    df["threshold_val"] <- snp_threshold[2,1]
+    return(df)
+}
+
+output_files <- list.files("./", pattern="*.csv")
+res <- lapply(output_files, function(x){
+    read_input(x)
+})
+res_df <- do.call("rbind", res)
