1d75416c24a8b7e171ff6f93d21d7e2f1ba4fc43
Abhishek Mishra <abhindra92@gmail.com>
2022-07-27 16:07:24 -0400

    updates cluster assignments

--- benchmark/clusters_to_assignment.R
+++ benchmark/clusters_to_assignment.R
@@ -1,6 +1,9 @@
-require(dplyr, tidyr, data.table, reshape2)
+library(dplyr)
+library(tidyr)
+library(data.table) 
+library(reshape2)
 
-#read the clustered data file, transpose it, and drop the text about the threshold
+#read the data file with row-wise clusters (e.g. output from transcluster)
 df_input <- t(read.table(file="transcluster_demo_output.csv", sep = ",", fill = T))
 df_input <- df_input[,-1]
 
@@ -10,3 +13,6 @@ colnames(df_input) <- paste0("cluster#", 1:ncol(df_input))
 df_clusters <- reshape2::melt(df_input)
 df_clusters[df_clusters==""] <- NA
 df_clusters <- na.omit(df_clusters)
+
+write.csv(df_clusters,"cluster_assignments.csv", row.names=FALSE)
+#output has all sample listed row-wise, along with their cluster ID
