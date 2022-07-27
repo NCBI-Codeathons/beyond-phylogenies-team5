fa188b6a3763266d2dbf4e4e9bbdb60bdc74ddbf
Abhishek Mishra <abhindra92@gmail.com>
2022-07-27 15:35:35 -0400

    Update clusters_to_assignment.R

--- benchmark/clusters_to_assignment.R
+++ benchmark/clusters_to_assignment.R
@@ -1,4 +1,4 @@
-require(tidyr, data.table)
+require(tidyr, data.table, reshape2)
 
 #read the clustered data file, transpose it, and drop the text about the threshold
 df_input <- t(read.table(file="transcluster_demo_output.csv", sep = ",", fill = T))
@@ -6,3 +6,5 @@ df_input <- df_input[,-1]
 
 #assign column ID to each column
 colnames(df_input) <- paste0("cluster#", 1:ncol(df_input))
+
+df_clusters <- reshape2::melt(df_input)
