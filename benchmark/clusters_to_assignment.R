448187123aa688759bcc786bc303bca6154487fb
Abhishek Mishra <abhindra92@gmail.com>
2022-07-27 15:26:53 -0400

    Create clusters_to_assignment.R

---
+++ benchmark/clusters_to_assignment.R
@@ -0,0 +1,8 @@
+require(tidyr, data.table)
+
+#read the clustered data file, transpose it, and drop the text about the threshold
+df_input <- t(read.table(file="transcluster_demo_output.csv", sep = ",", fill = T))
+df_input <- df_input[,-1]
+
+#assign column ID to each column
+colnames(df_input) <- paste0("cluster#", 1:ncol(df_input))
