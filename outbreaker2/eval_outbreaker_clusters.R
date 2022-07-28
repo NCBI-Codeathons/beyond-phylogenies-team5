731ae82ceeeb51059a1f9cbf8918d98d3c9cd884
tkchafin <tylerkchafin@gmail.com>
2022-07-28 21:50:13 +0100

    evaluating outbreaker with clusters

---
+++ outbreaker2/eval_outbreaker_clusters.R
@@ -0,0 +1,220 @@
+#!/usr/local/bin/Rscript
+
+suppressPackageStartupMessages({
+  library(ggplot2)
+  library(igraph)
+  library(plyr)
+  library(dplyr)
+  library(reshape2)
+  library(GetoptLong)
+})
+
+#################################################
+# Command-line arg parsing
+#################################################
+
+minP<-0.5
+maxP<-1.0
+step<-0.05
+out<-"out"
+maxSteps<-1000
+
+GetoptLong(
+  "rds=s", "Path to outbreaker2 results file (.rds)",
+  "links=s", "Path to links table",
+  "truePairs=s", "Path to known .pairs file",
+  "out=s", "Output file prefix",
+  "minP=f", "Minimum posterior probability threshold to test",
+  "maxP=f", "Maximum posterior probability threshold to test",
+  "step=f", "Step size for posterior probability range",
+  "maxSteps=f", "Maximum number of links to consider same cluster"
+)
+
+#################################################
+# Functions
+#################################################
+
+# get pairs above some posterior probability threshold 
+get_pairs <- function(wiw, thresh){
+  wiw <- wiw[wiw$generations.x == 1,]
+  wiw <- wiw[!is.na(wiw$fromT),]
+  wiw <- wiw[!is.na(wiw$toT),]
+  to<-wiw[wiw$support.x > thresh,"toT"]
+  from<-wiw[wiw$support.x > thresh,"fromT"]
+  pp<-wiw[wiw$support.x > thresh, "support.x"]
+  pairs <- data.frame("from"=from, "to"=to, "prob"=pp)
+  return(pairs)
+}
+
+get_pairs_from_clusters <- function(clusters){
+  df_clusters <- as.data.frame(cbind(clusterID=clust$membership, sample=clust$names))
+  df_clusters_rowwise <- df_clusters%>% group_by(clusterID) %>% summarize (N=n(),Clusters=paste (sample,collapse = ":"))
+  from<-list()
+  to<-list()
+  for (i in 1:nrow(df_clusters_rowwise)){
+    if (as.integer(df_clusters_rowwise[i,"N"]) > 1){
+      inds <- strsplit(as.character(df_clusters_rowwise[i,"Clusters"]), ":")[[1]]
+      pairs=combn(inds,2)
+      for (j in 1:ncol(pairs)){
+        from <- c(from, as.integer(pairs[1,j]))
+        to <- c(to, as.integer(pairs[2,j]))
+      }
+    }
+  }
+  pairs <- data.frame("from"=unlist(from), "to"=unlist(to))
+  return(pairs)
+}
+
+
+# returns some stats 
+# tp = # of true positives 
+# fn = # of false negatives (real positives not inferred)
+# fp = # of false positives (inferred pos not in true pairs)
+# tpr = Sensitivity or true positive rate = TP / (TP+FN)
+# fdr = false discover rate = FP / (TP+FP)
+# fnr = miss rate or false negative rate = FN / (TP+FN)
+# precision = precision or positive predictive value = TP / (TP+FP)
+# tp.prop = TP / total expected positives
+# fn.prop = FN / total expected positives
+# fp.prop = FP / total inferred positives
+# exp.pos = expected positives 
+# obs.pos = observed positives
+get_stats_pairs <- function(exp, obs, bidirectional=TRUE){
+  fn<-0
+  tp<-0
+  fp<-0
+  # get tp and fn
+  for(i in 1:nrow(exp)){
+    if (exp[i,1] %in% obs[,1]) {
+      if (exp[i,2] %in% obs[obs[,1]==exp[i,1],2]){
+        tp <- tp+1
+        next
+      }
+    }else{
+      if (bidirectional==TRUE){
+        if (exp[i,1] %in% obs[,2]) {
+          if (exp[i,2] %in% obs[obs[,2]==exp[i,1],1]){
+            tp <- tp+1
+            next
+          }
+        }
+      }
+    }
+  }
+  fp <- nrow(obs)-tp
+  fn <- nrow(exp)-tp
+  fnr <- fn/nrow(exp)
+  sens <- tp / (tp+fn)
+  fdr <- fp / (tp+fp)
+  prec <- tp / (tp+fp)
+  ret = data.frame(
+    "fp" = fp,
+    "tp" = tp,
+    "fn" = fn,
+    "exp.pos" = nrow(exp),
+    "obs.pos" = nrow(obs),
+    "tpr" = sens,
+    "fdr" = fdr,
+    "fnr" = fnr,
+    "precision" = prec
+  )
+  return(ret)
+}
+
+graph_from_pairs <- function(pairs){
+  edges <- pairs %>% select(from, to)
+  g<-graph_from_data_frame(edges, directed=FALSE)
+  return(g)
+}
+
+
+cluster_transmission_graph <- function(g, threshold, plot=FALSE, out="clusters"){
+  # get dist mat as # theoretical hosts between pairs
+  tips <- V(g)
+  dist<-shortest.paths(g, v=tips, to=tips)
+  dist[dist!=0] <- dist[dist!=0]-1 
+  adjacency <- dist
+  adjacency[dist<=threshold] <- 1
+  adjacency[dist>threshold] <- 0
+  diag(adjacency) <- 0
+  if (plot == TRUE){
+    pdf(paste0(out, "_clusterGraph.pdf"))
+    plot(as.undirected(graph.adjacency(adjacency)))
+    dev.off()
+  }
+  clusters<-fastgreedy.community(as.undirected(graph.adjacency(adjacency)))
+  return(clusters)
+}
+
+get_pairs_from_clusterList <- function(clusters){
+  from<-list()
+  to<-list()
+  for (i in nrow(clusters)){
+    print(clusters[[i]])
+    clust<-clusters[[i]]
+    if (len(clust) < 2){
+      next
+    }
+    pairs=combn(as.list(clust),2)
+    for (j in 1:ncol(pairs)){
+      from <- c(from, as.integer(pairs[1,j]))
+      to <- c(to, as.integer(pairs[2,j]))
+    }
+  }
+  pairs <- data.frame("from"=unlist(from), "to"=unlist(to))
+  return(pairs)
+}
+
+#################################################
+# Main
+#################################################
+print("Reading inputs...")
+true_pairs <- read.table(truePairs,
+                         header=T, 
+                         sep="\t")
+
+res <- readRDS(rds)
+ttree<-summary(res)$tree
+
+map <- read.table(links,
+                  header=T)
+
+ttree<-left_join(ttree, map, by=c("from"="from", "to"="to"))
+
+print("Testing across specified clustering thresholds...")
+results<-list()
+for (thresh in seq(minP, maxP, step)){
+  print(thresh)
+  pairs_temp <- get_pairs(ttree, thresh)
+  if(nrow(pairs_temp) < 1){
+    print("No observed pairs... Skipping")
+    next
+  }
+  g<-graph_from_pairs(pairs_temp)
+  clust<-cluster_transmission_graph(g, maxSteps, plot=FALSE)
+  pairs <- get_pairs_from_clusterList(clust)
+  stats <- get_stats_pairs(true_pairs, pairs)
+  stats["thresh"] <- thresh
+  results <- rbind(results, stats)
+}
+results <- rbind.fill(results)
+
+# make plots 
+print("Outputting results as _clustEval.pdf and _clustEval.tsv")
+
+s <- results %>% select(tpr, fdr, fnr, precision, thresh)
+sm <- melt(s, id="thresh")
+pdf(paste0(out, "_pairEval.pdf"))
+ggplot(sm, aes(x=thresh, y=value, color=variable)) +
+  theme_minimal() + 
+  geom_line(lwd=2)
+dev.off()
+  
+write.table(results,
+            paste0(out, "_pairEval.tsv"),
+            col.names=TRUE,
+            row.names=FALSE,
+            sep="\t",
+            quote=FALSE)
+
+print("Done!")
